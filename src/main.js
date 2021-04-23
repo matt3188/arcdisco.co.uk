75fb63932df3f7c6d392c86293681f7ade682def
Matt Coleman <matt.coleman@protonmail.com>
2021-04-23 16:23:00 +0100

    Added Google fonts

--- src/main.js
+++ src/main.js
@@ -5,4 +5,8 @@ import '~/assets/style/styles.scss'
 
 export default function (Vue, { head }) {
   Vue.component('Layout', DefaultLayout)
+  head.link.push({
+    rel: 'stylesheet',
+    href: encodeURI('https://fonts.googleapis.com/css2?family=Lato:wght@300&family=Open+Sans:wght@300&display=swap')
+  })
 }
